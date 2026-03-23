f11bf6e4c34691b026c2148a6fe1e47a0fe49cfb
ashley <129250977+ashleyjong@users.noreply.github.com>
2026-03-24 00:05:58 +0800

    Fix: Clean up main.js logic and optimise package.json build files

--- crypto-tracker/main.js
+++ crypto-tracker/main.js
@@ -5,7 +5,7 @@ const { autoUpdater } = require('electron-updater');
 function createWindow() {
   const win = new BrowserWindow({
     title: "Ashley's CryptoVault",
-    icon: path.join(__dirname, 'build/icon.ico'),
+    icon: path.join(__dirname, 'build/icon.ico'), 
     width: 1200,
     height: 800,
     webPreferences: {
@@ -21,8 +21,6 @@ function createWindow() {
   } else {
     win.loadFile(path.join(__dirname, 'dist', 'index.html'));
   }
-
-  win.loadURL(startUrl);
 
   win.webContents.on('did-fail-load', (event, errorCode, errorDescription) => {
     console.log(`Failed to load: ${errorDescription} (${errorCode})`);
